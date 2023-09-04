d51a2030c02a921d65155561975c3c1149030de7
Femosky <olufemiojeyemi@gmail.com>
2023-09-04 11:42:26 +0100

    Project 1 complete and working!

--- projects/scripts/project-one.js
+++ projects/scripts/project-one.js
@@ -16,60 +16,63 @@ resetButtonElement.addEventListener('click', () => {
 });
 
 let secondCounter;
-let = timerObject = {
+
+let timerObject = JSON.parse(localStorage.getItem('timerObject')) || {
     hour: 0,
     minute: 0,
     second: 0,
 };
 
+let defaultFormattedHour = String(timerObject.hour).padStart(2, '0');
+let deafaultFormattedMinute = String(timerObject.minute).padStart(2, '0');
+let defaultFormattedSecond = String(timerObject.second).padStart(2, '0');
+
+document.querySelector(
+    '.js-timer-output'
+).innerHTML = `<p class="timer-output-paragraph">${defaultFormattedHour}:${deafaultFormattedMinute}:${defaultFormattedSecond}</p>`;
+
+localStorage.setItem('timerObject', JSON.stringify(timerObject));
+
 function startTimer() {
-    let timerObjectJSON = localStorage.getItem('timerObject');
-
-    if (timerObjectJSON === null || timerObjectJSON === undefined) {
-        timerObject = JSON.parse(timerObjectJSON);
-    }
-
-    // let timerObject = JSON.parse(timerObjectJSON);
-
     document.querySelector('.js-button-holder').innerHTML = ``;
+
     document.querySelector(
         '.js-button-holder'
-    ).innerHTML = `<button class="js-stop-button">Stop</button>`;
+    ).innerHTML = `<button class="js-stop-button stop-button" data-type="inverted">Stop</button>`;
 
     secondCounter = setInterval(() => {
         timerObject.second++;
 
-        if (timerObject.second === 60 || timerObject.minute === 60) {
-            document.querySelector(
-                '.js-timer-output'
-            ).innerHTML = `<p>${timerObject.hour}:${timerObject.minute}:${timerObject.second}</p>`;
-        }
-
         if (timerObject.second === 60) {
             timerObject.second = 0;
-
-            setTimeout(() => {
-                timerObject.minute++;
-            }, 1000);
+            timerObject.minute++;
         }
-        if (timerObject.minute === 59) {
+
+        if (timerObject.minute === 60) {
             timerObject.minute = 0;
-
-            setTimeout(() => {
-                timerObject.hour++;
-            }, 1000 * 60);
+            timerObject.hour++;
         }
+
+        let formattedHour = String(timerObject.hour).padStart(2, '0');
+        let formattedMinute = String(timerObject.minute).padStart(2, '0');
+        let formattedSecond = String(timerObject.second).padStart(2, '0');
+
+        document.querySelector(
+            '.js-timer-output'
+        ).innerHTML = `<p class="timer-output-paragraph">${formattedHour}:${formattedMinute}:${formattedSecond}</p>`;
+
         localStorage.setItem('timerObject', JSON.stringify(timerObject));
     }, 1000);
+
     return secondCounter;
 }
 
 function stopTimer() {
     document.querySelector('.js-button-holder').innerHTML = ``;
-    // document.querySelector('.js-timer-output').innerHTML = ``;
+
     document.querySelector(
         '.js-button-holder'
-    ).innerHTML = `<button class="js-start-button">Start</button>`;
+    ).innerHTML = `<button class="js-start-button start-button" data-type="inverted">Start</button>`;
 
     clearInterval(secondCounter);
 }
@@ -79,7 +82,13 @@ function resetTimer() {
     timerObject.minute = 0;
     timerObject.second = 0;
 
+    let formattedHour = String(timerObject.hour).padStart(2, '0');
+    let formattedMinute = String(timerObject.minute).padStart(2, '0');
+    let formattedSecond = String(timerObject.second).padStart(2, '0');
+
     document.querySelector(
         '.js-timer-output'
-    ).innerHTML = `<p>${timerObject.hour}:${timerObject.minute}:${timerObject.second}</p>`;
+    ).innerHTML = `<p class="timer-output-paragraph">${formattedHour}:${formattedMinute}:${formattedSecond}</p>`;
+
+    localStorage.setItem('timerObject', JSON.stringify(timerObject));
 }
